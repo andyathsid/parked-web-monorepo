888048d9129e39881586e31e52acebd05b00701e
NekoMorie <davihanan11@gmail.com>
2024-10-31 23:53:45 +0700

    upload ML

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -19,36 +19,37 @@ class FormController extends Controller
             'fileModel2' => 'nullable|mimes:mp3,wav|max:61440',
             'fileModel3' => 'nullable|image|mimes:jpg,png|max:61440',
         ]);
-        \dd($request);
+        // \dd($request);
         $uploadedData = [
-            'vm-file' => 'https://raw.githubusercontent.com/andyathsid/parked-ml-backend/refs/heads/dev/data/raw/FB1cdaopmoe67M2605161917.wav',
-            // 'sd-file' => null,
-            // 'ni-file' => null
+            'vm-url' => 'https://drive.google.com/uc?export=download&id=1LXfVMjs16Mb4JTbTAStBUxtqrOB5G6Zf',
+            'hw-url' => null,
+            'ni-url' => null
         ];
-        // \dd($uploadedData);
-        // if ($request->hasFile('fileModel1')) {
-        //     $imagePath = $request->file('fileModel1')->store('file_modul1', 'public');
-        //     $uploadedData['vm_file'] = $imagePath;
-        // }
 
-        // if ($request->hasFile('fileModel2')) {
-        //     $audioPath = $request->file('fileModel2')->store('file_modul2', 'public');
-        //     $uploadedData['sd-file'] = $audioPath;
-        // } elseif ($request->input('recordedAudio')) {
-        //     $audioData = $request->input('recordedAudio');
-        //     $audioData = str_replace('data:audio/wav;base64,', '', $audioData);
-        //     $audioData = base64_decode($audioData);
-        //     $audioPath = 'audios/recorded-' . time() . '.wav';
-        //     Storage::disk('public')->put($audioPath, $audioData);
-        //     $uploadedData['sd-file'] = $audioPath;
-        // }
+        if ($request->hasFile('fileModel1')) {
+            $imagePath = $request->file('fileModel1')->store('file_modul1', 'public');
+            $uploadedData['sd-file'] = url('storage/' . $imagePath);
+        }
 
-        // if ($request->hasFile('fileModel3')) {
-        //     $fileModel3 = $request->file('fileModel3')->store('file_modul3', 'public');
-        //     $uploadedData['ni-file'] = $fileModel3;
-        // }
-        // \dd($uploadedData);
+        if ($request->hasFile('fileModel2')) {
+            $audioPath = $request->file('fileModel2')->store('file_modul2', 'public');
+            $uploadedData['vm-url'] = url('storage/' . $audioPath);
+        } elseif ($request->input('recordedAudio')) {
+            $audioData = $request->input('recordedAudio');
+            $audioData = str_replace('data:audio/wav;base64,', '', $audioData);
+            $audioData = base64_decode($audioData);
+            $audioPath = 'audios/recorded-' . time() . '.wav';
+            Storage::disk('public')->put($audioPath, $audioData);
+            $uploadedData['vm-url'] = url('storage/' . $audioPath);
+        }
 
+        if ($request->hasFile('fileModel3')) {
+            $fileModel3 = $request->file('fileModel3')->store('file_modul3', 'public');
+            $uploadedData['ni-file'] = url('storage/' . $fileModel3);
+        }
+
+
+        // Kirim POST request
         $response = Http::post('https://91qptmtp7e.execute-api.ap-southeast-1.amazonaws.com/test/predict', $uploadedData);
 
         // Periksa respons dari API
@@ -57,6 +58,7 @@ class FormController extends Controller
             return response()->json([
                 'success' => true,
                 'data' => $response->json(),
+                'upload' => $uploadedData
             ]);
         } else {
             // Respons gagal
@@ -66,8 +68,8 @@ class FormController extends Controller
                 'error' => $response->body(),
             ], $response->status());
         }
-
-        \dd($uploadedData);
+        echo $response;
+        \dd();
 
         // cek Json
         return response()->json([
