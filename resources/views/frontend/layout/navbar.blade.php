8751f0f0113e6d8baaf0b200995370f944ed99a1
NekoMorie <davihanan11@gmail.com>
2024-11-01 23:46:35 +0700

    minor

--- resources/views/frontend/layout/navbar.blade.php
+++ resources/views/frontend/layout/navbar.blade.php
@@ -11,7 +11,8 @@
                     <a class="nav-link {{ Request::is('/') ? 'actives' : '' }}" href="/">Beranda</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{ Request::is('resources') ? 'actives' : '' }}" href="/resources">Sumber Daya</a>
+                    <a class="nav-link {{ Request::is('resources') ? 'actives' : '' }}" href="/resources">Sumber
+                        Daya</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link {{ Request::is('information') ? 'actives' : '' }}"
@@ -43,8 +44,8 @@
             <div class="dropdown d-none d-lg-block">
                 <button class="btn btn-user-dropdown" type="button" id="userDropdown" data-bs-toggle="dropdown"
                     aria-expanded="false">
-                    <img src="{{ $user->photo ? Storage::url($user->photo) : asset($user->google_photo) }}" alt=""
-                        class="border rounded-circle" width="50px">
+                    <img src="{{ $user->photo ? Storage::url($user->photo) : asset($user->google_photo) }}" alt="User Photo"
+                        class="border rounded-circle" width="50" height="50" style="object-fit: cover;">
                 </button>
                 <ul class="dropdown-menu dropdown-menu-lg-end" aria-labelledby="userDropdown">
                     <li><a class="dropdown-item" href="/profile">Profil</a></li>
