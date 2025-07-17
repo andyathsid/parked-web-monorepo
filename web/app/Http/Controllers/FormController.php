bf089867d548f6fc89da5f09c39ac6a7b171c9a6
andyathsid <andakaraas@gmail.com>
2025-07-17 10:25:35 +0700

    fix(form): update file validation rules

--- web/app/Http/Controllers/FormController.php
+++ web/app/Http/Controllers/FormController.php
@@ -17,15 +17,21 @@ class FormController extends Controller
     public function pastientUpload(Request $request)
     {
         $request->validate([
-            'fileModel1' => 'nullable|image|mimes:jpg,png|max:61440',
-            'fileModel2' => 'nullable|mimes:wav|max:61440',
-            'fileModel3' => 'nullable|image|mimes:jpg,png|max:61440',
-            'audioUpload' => 'nullable|mimes:,wav|max:61440'
+            'fileModel1' => 'nullable|image|mimes:jpg,png|max:6144',
+            'fileModel2' => 'nullable|max:30720',
+            'fileModel3' => 'nullable|image|mimes:jpg,png|max:6144',
+            'recordedAudio' => 'nullable'
         ]);
 
-        // \dd($request);
-        if (!$request->hasFile('fileModel1') && !$request->hasFile('fileModel2') && !$request->input('recordedAudio') && !$request->hasFile('fileModel3')) {
+        // \dd($request['recordedAudio']);
+        if (!$request->hasFile('fileModel1') && !$request->hasFile('fileModel2') && !$request->hasFile('recordedAudio') && !$request->hasFile('fileModel3')) {
             session()->flash('gagal', 'Anda harus mengunggah minimal satu file!');
+            return redirect()->back();
+        }
+
+        // Tambahkan logika jika kedua file terisi
+        if ($request->hasFile('fileModel2') && $request->hasFile('recordedAudio')) {
+            session()->flash('gagal', 'Silakan pilih salah satu: Tes Rekam Audio atau Upload File. Kedua opsi tidak dapat digunakan bersamaan.');
             return redirect()->back();
         }
 
@@ -43,8 +49,8 @@ class FormController extends Controller
         if ($request->hasFile('fileModel2')) {
             $audioPath = $request->file('fileModel2')->store('file_modul2', 'public');
             $uploadedData['vm-url'] = url('storage/' . $audioPath);
-        } elseif ($request->input('recordedAudio')) {
-            $audioData = $request->input('recordedAudio');
+        } elseif ($request->hasFile('recordedAudio')) {
+            $audioData = $request->file('recordedAudio');
             $audioData = str_replace('data:audio/wav;base64,', '', $audioData);
             $audioData = base64_decode($audioData);
             $audioPath = 'audios/recorded-' . time() . '.wav';
@@ -57,35 +63,57 @@ class FormController extends Controller
             $uploadedData['ni-url'] = url('storage/' . $fileModel3);
         }
 
-        $uploadedData['hw-url'] = 'https://github.com/andyathsid/parked-ml-dev/blob/main/hand-writing-detection/data/raw/NewHandPD/HealthySpiral/sp1-H1.jpg?raw=true';
-        // $uploadedData['vm-url'] = 'https://drive.google.com/uc?export=download&id=1LXfVMjs16Mb4JTbTAStBUxtqrOB5G6Zf';
-        $uploadedData['vm-url'] = 'https://github.com/andyathsid/parked-ml-backend/blob/dev-alt/voice-measurements-detection-service/data/raw/FB1cdaopmoe67M2605161917.wav?raw=true';
-        // Kirim POST request
-        $response = Http::post(config('services.api.diagnosis'), $uploadedData);
+        // dd($uploadedData);
+        // $uploadedData['hw-url'] = 'https://parked.my.id/storage/file_modul1/17UtzjQEtaKml7vF92Vn3qdvEZEcEk2zfngdiCxl.jpg';
+        // $uploadedData['vm-url'] = 'https://parked.my.id/storage/file_modul2/099mlDBsKJVDhjCJNWtr4IfgPnYYUcDTCCWYzumd.wav';
+        // $uploadedData['ni-url'] = 'https://parked.my.id/storage/file_modul3/0Nc2if8qqObwz25Qv1o8YlpIY9wnesxIB6UAEhBC.jpg';
+
+        $filteredData = array_filter($uploadedData, function ($value) {
+            return !is_null($value);
+        });
+        // dd($filteredData);
+
+        $response = Http::post(config('services.api.diagnosis'), $filteredData);
 
         if ($response->successful()) {
             $apiResponse = $response->json();
-            // \dd();
+            // \dd($fileModel1);
             // if ($request->fileModel3 != NULL) {
-            return response()->json([
-                'success' => true,
-                'data' => $apiResponse,
-                'upload' => $uploadedData
-            ]);
-            DB::table('form')->insert([
-                'user_id' => auth()->id(),
-                'file_diagnosa1' => $fileModel1 ?? null,
-                'file_diagnosa2' => $audioPath ?? null,
-                'file_diagnosa3' => $fileModel3 ?? null,
-                'hasil_diagnosa1' => $apiResponse['hw-result'] ?? null,
-                'hasil_diagnosa2' => $apiResponse['vm-result'] ?? null,
-                'hasil_diagnosa3' => $apiResponse['ni-result'] ?? null,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]);
-            session(['apiResponse' => $apiResponse]);
+            // return response()->json([
+            //     'success' => true,
+            //     'data' => $apiResponse,
+            //     'upload' => $uploadedData
+            // ]);
+            // if (!isset($apiResponse['hw-result'], $apiResponse['vm-result'], $apiResponse['ni-result'])) {
+            //     return redirect()->route('form-diagnosa')->with('gagal', 'Format respons dari server tidak valid.');
+            // }
+            if ($apiResponse == true || $apiResponse == false) {
+                if ($apiResponse == NULL) {
+                    return redirect()->route('form-diagnosa')->with('gagal', 'Kesalahan dalam membaca data!');
+                } else {
+                    DB::table('form')->insert([
+                        'user_id' => auth()->id(),
+                        'file_diagnosa1' => $fileModel1 ?? null,
+                        'file_diagnosa2' => $audioPath ?? null,
+                        'file_diagnosa3' => $fileModel3 ?? null,
+                        'hasil_diagnosa1' => $apiResponse['hw-result'] ?? null,
+                        'hasil_diagnosa2' => $apiResponse['vm-result'] ?? null,
+                        'hasil_diagnosa3' => $apiResponse['ni-result'] ?? null,
+                        'created_at' => now(),
+                        'updated_at' => now(),
+                    ]);
+                    session(['apiResponse' => $apiResponse]);
 
-            return redirect()->route('result')->with('success', 'Data berhasil diproses!');
+                    // Redirect ke halaman sukses
+                    return response()->json([
+                        'redirect' => route('result') // Ganti dengan rute yang sesuai
+                    ]);
+                    // return redirect()->route('result')->with('success', 'Data berhasil diproses!');
+                }
+            } else {
+                return redirect()->route('form-diagnosa')->with('gagal', 'Server Gagal Merespons');
+            }
+            // }
         } else {
             return redirect()->route('form-diagnosa')->with('gagal', 'Data yang dikirim Gagal Di proses!');
         }
